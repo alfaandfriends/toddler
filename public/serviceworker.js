a170e662f9751f8b71cd6ba2258fc0e2bd809882
Fijunas <abdulraof628@gmail.com>
2024-09-03 16:50:18 +0800

    fix offline

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,8 +1,8 @@
 var staticCacheName = "pwa-v" + new Date().getTime();
 var filesToCache = [
     '/offline',
-    '/css/app.css',
-    '/js/app.js',
+    '/build/assets/app-BLFGSuNC.css',
+    '/build/assets/app-3njathAT.js',
     '/images/icons/icon-72x72.png',
     '/images/icons/icon-96x96.png',
     '/images/icons/icon-128x128.png',
