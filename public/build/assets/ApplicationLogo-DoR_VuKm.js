a903269cae1ad46acf3f2c1a5528f646b2ba0898
Fijunas <abdulraof628@gmail.com>
2024-09-06 11:34:55 +0800

    rename platform title

--- public/build/assets/ApplicationLogo-DoR_VuKm.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as c,f as e}from"./app-AZ-p8ftd.js";const n={},t={src:"/images/icons/icon_toddler_512.png",alt:""};function s(r,_){return c(),e("img",t)}const i=o(n,[["render",s]]);export{i as A};
