33650f6e19637fb849dd3cc640717a2534924240
Fijunas <abdulraof628@gmail.com>
2024-09-06 09:30:44 +0800

    change icon

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'name' => 'Little Scientist Toddler',
+    'name' => 'Toddler',
     'manifest' => [
-        'name' => 'Little Scientist Toddler',
-        'short_name' => 'Little Scientist Toddler',
+        'name' => 'Toddler',
+        'short_name' => 'Toddler',
         'start_url' =>  env('APP_URL',''),
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
@@ -13,49 +13,49 @@ return [
         'status_bar'=> 'black',
         'icons' => [
             '72x72' => [
-                'path' => '/images//icons/icon_toddler_72.png',
+                'path' => '/images/icons/icon_toddler_72.png',
                 'purpose' => 'any'
             ],
             '96x96' => [
-                'path' => '/images//icons/icon_toddler_96.png',
+                'path' => '/images/icons/icon_toddler_96.png',
                 'purpose' => 'any'
             ],
             '128x128' => [
-                'path' => '/images//icons/icon_toddler_128.png',
+                'path' => '/images/icons/icon_toddler_128.png',
                 'purpose' => 'any'
             ],
             '144x144' => [
-                'path' => '/images//icons/icon_toddler_144.png',
+                'path' => '/images/icons/icon_toddler_144.png',
                 'purpose' => 'any'
             ],
             '152x152' => [
-                'path' => '/images//icons/icon_toddler_152.png',
+                'path' => '/images/icons/icon_toddler_152.png',
                 'purpose' => 'any'
             ],
             '192x192' => [
-                'path' => '/images//icons/icon_toddler_192.png',
+                'path' => '/images/icons/icon_toddler_192.png',
                 'purpose' => 'any'
             ],
             '384x384' => [
-                'path' => '/images//icons/icon_toddler_384.png',
+                'path' => '/images/icons/icon_toddler_384.png',
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => '/images//icons/icon_toddler_512.png',
+                'path' => '/images/icons/icon_toddler_512.png',
                 'purpose' => 'any'
             ],
         ],
         'splash' => [
-            '640x1136' => '/images//icons/icon_toddler_512.png',
-            '750x1334' => '/images//icons/icon_toddler_512.png',
-            '828x1792' => '/images//icons/icon_toddler_512.png',
-            '1125x2436' => '/images//icons/icon_toddler_512.png',
-            '1242x2208' => '/images//icons/icon_toddler_512.png',
-            '1242x2688' => '/images//icons/icon_toddler_512.png',
-            '1536x2048' => '/images//icons/icon_toddler_512.png',
-            '1668x2224' => '/images//icons/icon_toddler_512.png',
-            '1668x2388' => '/images//icons/icon_toddler_512.png',
-            '2048x2732' => '/images//icons/icon_toddler_512.png',
+            '640x1136' => '/images/icons/icon_toddler_512.png',
+            '750x1334' => '/images/icons/icon_toddler_512.png',
+            '828x1792' => '/images/icons/icon_toddler_512.png',
+            '1125x2436' => '/images/icons/icon_toddler_512.png',
+            '1242x2208' => '/images/icons/icon_toddler_512.png',
+            '1242x2688' => '/images/icons/icon_toddler_512.png',
+            '1536x2048' => '/images/icons/icon_toddler_512.png',
+            '1668x2224' => '/images/icons/icon_toddler_512.png',
+            '1668x2388' => '/images/icons/icon_toddler_512.png',
+            '2048x2732' => '/images/icons/icon_toddler_512.png',
         ],
         // 'shortcuts' => [
         //     [
@@ -63,7 +63,7 @@ return [
         //         'description' => 'Shortcut Link 1 Description',
         //         'url' => '/',
         //         'icons' => [
-        //             "src" => "/images/images//icons/icon-72x72.png",
+        //             "src" => "/imagesimages//icons/icon-72x72.png",
         //             "purpose" => "any"
         //         ]
         //     ],
